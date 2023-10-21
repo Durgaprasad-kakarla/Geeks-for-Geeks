0307911ff0752252fd9c0dd18b0e74cdfac203ca
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-10-21 13:00:59 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sum of all divisors from 1 to n/sum-of-all-divisors-from-1-to-n.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+
+class Solution:
+    def sumOfDivisors(self, n):
+    	#code here 
+        sm=0
+        for i in range(1,n+1):
+            sm+=(n//i)*i
+        return sm
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.sumOfDivisors(N)
+        print(ans)
+# } Driver Code Ends
