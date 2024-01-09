8763b292968df75ba3ec15786a4e55c9fbc17f76
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-01-09 17:57:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.py
@@ -0,0 +1,60 @@
+#User function Template for python3
+
+class Solution:
+    def search(self, pat, txt):
+        # code here
+        def computeLPSArray(pat):
+            m = len(pat)
+            lps = [0] * m
+            length = 0
+            i = 1
+            while i < m:
+                if pat[i] == pat[length]:
+                    length += 1
+                    lps[i] = length
+                    i += 1
+                else:
+                    if length != 0:
+                        length = lps[length - 1]
+                    else:
+                        lps[i] = 0
+                        i += 1
+            return lps
+        indices = []
+        m = len(pat)
+        n = len(txt)
+        lps = computeLPSArray(pat)
+        i = 0
+        j = 0
+        while i < n:
+            if pat[j] == txt[i]:
+                i += 1
+                j += 1
+            if j == m:
+                indices.append(i - j + 1)
+                j = lps[j - 1]
+            elif i < n and pat[j] != txt[i]:
+                if j != 0:
+                    j = lps[j - 1]
+                else:
+                    i += 1
+        return indices
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        patt = input().strip()
+        ob = Solution()
+        ans = ob.search(patt, s)
+        if len(ans)==0:
+            print(-1,end="")
+        for value in ans:
+            print(value,end = ' ')
+        print()
+# } Driver Code Ends
