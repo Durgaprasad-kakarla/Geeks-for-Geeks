d29f9f7e0b10963783ffcde24371abce90276a0b
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-01-30 00:28:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find the number of islands/find-the-number-of-islands.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+import sys
+sys.setrecursionlimit(10**8)
+class Solution:
+    def numIslands(self,grid):
+        #code here
+        n=len(grid)
+        m=len(grid[0])
+        vis=[[0 for i in range(m)] for j in range(n)]
+        def dfs(row,col,vis,grid):
+            vis[row][col]=1
+            dr=[-1,-1,-1,0,1,1,1,0]
+            dc=[-1,0,1,1,1,0,-1,-1]
+            for i in range(8):
+                nrow=row+dr[i]
+                ncol=col+dc[i]
+                if nrow>=0 and ncol>=0 and nrow<n and ncol<m and not vis[nrow][ncol] and grid[nrow][ncol]==1:
+                    dfs(nrow,ncol,vis,grid)
+        cnt=0
+        for i in range(n):
+            for j in range(m):
+                if not vis[i][j] and grid[i][j]==1:
+                    cnt+=1
+                    dfs(i,j,vis,grid)
+        return cnt
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    for _ in range(int(input())):
+        n,m=map(int,input().strip().split())
+        grid=[]
+        for i in range(n):
+            grid.append([int(i) for i in input().strip().split()])
+        obj=Solution()
+        print(obj.numIslands(grid))
+# } Driver Code Ends
