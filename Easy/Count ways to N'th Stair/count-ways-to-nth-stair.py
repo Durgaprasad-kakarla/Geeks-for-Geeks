de35ad54ac7b059372d7ae553ea75e0a73e21da3
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-04-06 00:47:59 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count ways to N'th Stair/count-ways-to-nth-stair.py
+++ Easy/Count ways to N'th Stair/count-ways-to-nth-stair.py
@@ -5,7 +5,7 @@ class Solution:
     #Function to count number of ways to reach the nth stair
     #when order does not matter.
     def countWays(self, n):
-        return (n//2+1)%(10**9+7)
+        return (n//2+1)
         
 
 
