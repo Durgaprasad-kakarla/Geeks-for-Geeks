52e9e1f21601472e46677e4f57aa88b087d1efc7
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-04-12 22:35:29 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if Linked List is Palindrome/check-if-linked-list-is-palindrome.py
@@ -0,0 +1,92 @@
+#User function Template for python3
+'''
+	Your task is to check if given linkedlist
+	is a pallindrome or not.
+	
+	Function Arguments: head (reference to head of the linked list)
+	Return Type: boolean , no need to print just return True or False.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+
+	Contributed By: Nagendra Jha
+'''
+#Function to check whether the list is palindrome.
+class Solution:
+    def isPalindrome(self, head):
+        #code here
+        if not head.next:
+            return 1
+        slow,fast=head,head
+        while fast and fast.next:
+            prev=slow
+            slow=slow.next
+            fast=fast.next.next
+        prev.next=None
+        temp=slow
+        dummy=None
+        while temp:
+            curr=temp.next
+            temp.next=dummy
+            dummy=temp
+            temp=curr
+        while head and dummy:
+            if head.data!=dummy.data:
+                return 0
+            head=head.next
+            dummy=dummy.next
+        return 1
+        
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node 
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+
+        if Solution().isPalindrome(a.head):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
