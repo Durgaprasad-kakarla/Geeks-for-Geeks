7f6fb19dd3da22802e2f4e2f7328a45400bfcf3e
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-05-18 15:27:40 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Queue using stack/queue-using-stack.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Queue:
+    def __init__(self):
+        self.stack1=[]
+        self.stack2=[]
+    def enqueue(self,X):
+        self.stack1.append(X)
+    def dequeue(self):
+        if self.stack2:
+            return self.stack2.pop()
+        else:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+            if not self.stack2:
+                return -1
+            return self.stack2.pop()
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        ob=Queue()
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        i = 0
+        while i<len(a):
+            if a[i] == 1:
+                ob.enqueue(a[i+1])
+                i+=1
+            else:
+                print(ob.dequeue(),end=" ")
+            i+=1
+
+        print()
+# } Driver Code Ends
