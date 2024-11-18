b17516a5b44c43c6067291e5275f5585261bf8ed
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-11-18 06:58:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.py
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.py
@@ -4,21 +4,25 @@ class Solution:
     def findPath(self, mat: List[List[int]]) -> List[str]:
         # code here
         n,m=len(mat),len(mat[0])
-        ans=[]
-        def func(row,col,vis,s):
-            if row==n-1 and col==m-1 and mat[row][col]==1:
+        def dfs(row,col,vis,mat,s):
+            if row==n-1 and col==m-1:
                 ans.append(s)
                 return 
-            if row<0 or row>=n or col<0 or col>=m or mat[row][col]==0 or vis[row][col]==1:
-                return 
             vis[row][col]=1
-            down=func(row+1,col,vis,s+'D')
-            up=func(row-1,col,vis,s+'U')
-            left=func(row,col-1,vis,s+'L')
-            right=func(row,col+1,vis,s+'R')
+            dr=[-1,0,1,0]
+            dc=[0,-1,0,1]
+            dir={0:'U',1:'L',2:'D',3:'R'}
+            for i in range(4):
+                nrow=row+dr[i]
+                ncol=col+dc[i]
+                if nrow>=0 and ncol>=0 and nrow<n and ncol<m and not vis[nrow][ncol] and mat[nrow][ncol]==1:
+                    dfs(nrow,ncol,vis,mat,s+dir[i])
             vis[row][col]=0
+        ans=[]
         vis=[[0 for i in range(m)] for j in range(n)]
-        func(0,0,vis,'')
+        if mat[0][0]==0:
+            return []
+        dfs(0,0,vis,mat,"")
         return ans
 
 
@@ -39,5 +43,6 @@ if __name__ == "__main__":
             print(-1)
         else:
             print(" ".join(result))
+        print("~")
 
 # } Driver Code Ends
