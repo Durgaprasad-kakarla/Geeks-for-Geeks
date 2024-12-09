f80a0c4637f72b2f91978242eb50edfb1b8de723
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-12-09 11:11:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Insert Interval/insert-interval.py
+++ Difficulty: Medium/Insert Interval/insert-interval.py
@@ -8,37 +8,19 @@
 class Solution:
     def insertInterval(self, intervals, newInterval):
         # Code here
+        intervals.append(newInterval)
         intervals.sort()
-        n=len(intervals)
-        ans=[]
-        start,end,flag=-1,-1,1
-        for i in range(n):
+        ans,n=[],len(intervals)
+        start,end=intervals[0]
+        for i in range(1,n):
             s,e=intervals[i]
-            if (newInterval[0]>=s and newInterval[0]<=e) or (newInterval[1]>=s and newInterval[1]<=e) or (newInterval[1]>=e and newInterval[0]<=s):
-                start=min(s,newInterval[0])
-                end=max(e,newInterval[1])
-                newInterval[0],newInterval[1]=start,end
-                flag=0
+            if end>=s:
+                end=max(end,e)
             else:
-                if start==-1 and end==-1:
-                    if newInterval[0]<s and newInterval[1]<s and flag:
-                        ans.append(newInterval)
-                        ans.append([s,e])
-                        flag=0
-                    else:
-                        ans.append([s,e])
-                else:
-                    ans.append([start,end])
-                    ans.append([s,e])
-                    start,end=-1,-1
-        if flag==1:
-            ans.append(newInterval)
-        if start==-1 and end==-1:
-            return ans
+                ans.append([start,end])
+                start,end=intervals[i]
         ans.append([start,end])
         return ans
-                    
-
 #{ 
  # Driver Code Starts.
 if __name__ == '__main__': 
