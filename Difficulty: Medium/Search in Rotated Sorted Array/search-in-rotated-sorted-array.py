f52f6fe70e451e202a37c165a3dbd5ba6a6e8f0d
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-12-14 00:50:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.py
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.py
@@ -1,25 +1,25 @@
 #User function Template for python3
 
 class Solution:
-    def search(self,arr,key):
+    def search(self,arr,target):
         # Complete this function
-        l,r=0,len(arr)-1
+        n=len(arr)
+        l,r=0,n-1
         while l<=r:
             mid=(l+r)//2
-            if arr[mid]==key:
+            if arr[mid]==target:
                 return mid
             if arr[l]<=arr[mid]:
-                if arr[l]<=key and arr[mid]>key:
+                if arr[l]<=target and arr[mid]>target:
                     r=mid-1
                 else:
                     l=mid+1
             else:
-                if arr[mid]<key and arr[r]>=key:
+                if arr[mid]<target and arr[r]>=target:
                     l=mid+1
                 else:
                     r=mid-1
         return -1
-    
 
 #{ 
  # Driver Code Starts
@@ -33,5 +33,6 @@ if __name__ == '__main__':
         k = int(input())
         ob = Solution()
         print(ob.search(A, k))
+        print("~")
 
 # } Driver Code Ends
