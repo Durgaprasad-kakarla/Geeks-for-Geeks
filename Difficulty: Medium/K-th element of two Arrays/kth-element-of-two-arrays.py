b712a49abcc177c7fd89ab6e2755a9d4cff2c47e
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-12-16 06:34:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
@@ -1,38 +1,34 @@
 #User function Template for python3
 
+
 class Solution:
-    def kthElement(self, k, arr1, arr2):
-        n1,n2=len(arr1),len(arr2)
-        if n1>n2:
-            return self.kthElement(k,arr2,arr1)
-        left=k-1
-        l,r=0,n1
-        while l<=r:
-            mid1=(l+r)//2
-            mid2=left-mid1
-            if mid2<0:
-                r=mid1-1
-                continue
-            if mid2>n2:
-                l=mid1+1
-                continue
-            l1,l2,r1,r2=-float('inf'),-float('inf'),float('inf'),float('inf')
-            if mid1<n1:
-                r1=arr1[mid1]
-            if mid2<n2:
-                r2=arr2[mid2]
-            if mid1-1>=0:
-                l1=arr1[mid1-1]
-            if mid2-1>=0:
-                l2=arr2[mid2-1]
-            if l1<=r2 and l2<=r1:
-                return min(r1,r2)
-            elif l1>r2:
-                r=mid1-1
+
+    def kthElement(self, arr, brr, k):
+        l,r,n,m=0,0,len(arr),len(brr)
+        while l<n and r<m:
+            if arr[l]<=brr[r]:
+                k-=1
+                if k==0:
+                    return arr[l]
+                l+=1
             else:
-                l=mid1+1
-        return -1
-        
+                k-=1
+                if k==0:
+                    return brr[r]
+                r+=1
+        while l<n:
+            k-=1
+            if k==0:
+                return arr[l]
+            l+=1
+        while r<m:
+            k-=1
+            if k==0:
+                return brr[r]
+            r+=1
+            
+         
+
 
 
 #{ 
@@ -50,8 +46,8 @@ def main():
         a = [int(x) for x in input().strip().split()]
         b = [int(x) for x in input().strip().split()]
         ob = Solution()
-        print(ob.kthElement(k, a, b))
-
+        print(ob.kthElement(a, b, k))
+        print("~")
         T -= 1
 
 
