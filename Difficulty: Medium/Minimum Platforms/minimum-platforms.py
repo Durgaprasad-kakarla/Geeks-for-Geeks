533bcfa9b007555392996aec4a0d75c6907eea1a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-12-17 11:45:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+import heapq
+class Solution:    
+    #Function to find the minimum number of platforms required at the
+    #railway station such that no train waits.
+    def minimumPlatform(self,arr,dep):
+        # code here
+        heap=[]
+        n=len(arr)
+        arr_dep=[]
+        for i in range(n):
+            arr_dep.append([arr[i],dep[i]])
+        arr_dep.sort()
+        maxi=0
+        for i in range(n):
+            while heap and heap[0]<arr_dep[i][0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap,arr_dep[i][1])
+            maxi=max(maxi,len(heap))
+        return maxi
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        arrival = list(map(int, input().strip().split()))
+        departure = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.minimumPlatform(arrival, departure))
+        print("~")
+
+# } Driver Code Ends
