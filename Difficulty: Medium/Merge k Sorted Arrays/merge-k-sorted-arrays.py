9a1b174d70dd2f7c902c30ce323145f2b465adbb
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-12-23 23:51:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+import heapq
+class Solution:
+    #Function to merge k sorted arrays.
+    def mergeKArrays(self, arr, K):
+        # code here
+        # return merged list
+        heap=[]
+        for lst in arr:
+            for i in lst:
+                heapq.heappush(heap,i)
+        lst=[]
+        while heap:
+            lst.append(heapq.heappop(heap))
+        return lst
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        numbers=[[ 0 for _ in range(n) ] for _ in range(n) ]
+        line=input().strip().split()
+        for i in range(n):
+            for j in range(n):
+                numbers[i][j]=int(line[i*n+j])
+        ob = Solution();
+        merged_list=ob.mergeKArrays(numbers, n)
+        for i in merged_list:
+            print(i,end=' ')
+        print()
+        print("~")
+# } Driver Code Ends
