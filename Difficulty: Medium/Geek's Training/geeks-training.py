2e7012c689225a802887829e7a95068ebef54e4c
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-01-19 09:07:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.py
+++ Difficulty: Medium/Geek's Training/geeks-training.py
@@ -4,23 +4,39 @@ class Solution:
     def maximumPoints(self, arr):
         # Code here
         n=len(arr)
-        def func(day,prev):
-            if day==0:
-                maxi=0
-                for i in range(1,4):
-                    if i!=prev:
-                        maxi=max(maxi,arr[0][i-1])
-                return maxi
-            if dp[day][prev]!=-1:
-                return dp[day][prev]
+        dp=[[0 for i in range(4)] for j in range(n)]
+        for i in range(4):
             maxi=0
-            for i in range(1,4):
-                if i!=prev:
-                    maxi=max(maxi,arr[day][i-1]+func(day-1,i))
-            dp[day][prev]=maxi
-            return maxi
-        dp=[[-1 for i in range(4)] for j in range(n)]
-        return func(n-1,0)
+            for j in range(3):
+                if i!=j:
+                    maxi=max(maxi,arr[0][j])
+            dp[0][i]=maxi
+        for day in range(1,n):
+            for i in range(4):
+                maxi=0
+                for j in range(3):
+                    if i!=j:
+                        maxi=max(maxi,arr[day][j]+dp[day-1][j])
+                dp[day][i]=maxi
+        return (dp[n-1][3])
+            
+        # def func(day,prev):
+        #     if day==0:
+        #         maxi=0
+        #         for i in range(1,4):
+        #             if i!=prev:
+        #                 maxi=max(maxi,arr[0][i-1])
+        #         return maxi
+        #     if dp[day][prev]!=-1:
+        #         return dp[day][prev]
+        #     maxi=0
+        #     for i in range(1,4):
+        #         if i!=prev:
+        #             maxi=max(maxi,arr[day][i-1]+func(day-1,i))
+        #     dp[day][prev]=maxi
+        #     return maxi
+        # dp=[[-1 for i in range(4)] for j in range(n)]
+        # return func(n-1,0)
 
 #{ 
  # Driver Code Starts
