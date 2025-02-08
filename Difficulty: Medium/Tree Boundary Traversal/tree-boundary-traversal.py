5cda73a968d009f7abb54089f630dcac7293d2a4
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-02-08 12:50:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.py
@@ -0,0 +1,158 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+class Solution:
+    def boundaryTrav(self, root, result, side):
+        cur_root  = root
+        tmp = []
+        while cur_root:
+            
+            if cur_root.left or cur_root.right:
+                tmp.append(cur_root.data)
+                
+            if side == "left":
+                if cur_root.left:
+                    cur_root = cur_root.left
+                else:
+                    cur_root = cur_root.right
+            else:
+                if cur_root.right:
+                    cur_root = cur_root.right
+                else:
+                    cur_root = cur_root.left
+
+        if side == "left":
+            result.extend(tmp)
+        else:
+            result.extend(tmp[::-1])
+            
+    def leafs(self, root, result):
+        queue = [root]
+        tmp = []
+        while queue:
+            cur = queue.pop()
+            
+            if not cur.left and not cur.right:
+                tmp.append(cur.data)
+                
+            if cur.left:
+                queue.append(cur.left)
+            if cur.right:
+                queue.append(cur.right)
+        result.extend(tmp[::-1])
+    def boundaryTraversal(self, root):
+        # Code here
+        result = []
+        if not root:
+            return result
+        if root.left or root.right:
+            result.append(root.data)
+        self.boundaryTrav(root.left, result, "left")
+        self.leafs(root, result)
+        self.boundaryTrav(root.right, result, "right")
+        return result
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# function should return a list containing the boundary view of the binary tree
+#{
+#  Driver Code Starts
+import sys
+
+import sys
+
+sys.setrecursionlimit(100000)
+#Contributed by Sudarshan Sharma
+from collections import deque
+
+
+# Tree Node
+class Node:
+
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+# Function to Build Tree
+def buildTree(s):
+    #Corner Case
+    if (len(s) == 0 or s[0] == "N"):
+        return None
+
+    # Creating list of strings from input
+    # string after spliting by space
+    ip = list(map(str, s.split()))
+
+    # Create the root of the tree
+    root = Node(int(ip[0]))
+    size = 0
+    q = deque()
+
+    # Push the root to the queue
+    q.append(root)
+    size = size + 1
+
+    # Starting from the second element
+    i = 1
+    while (size > 0 and i < len(ip)):
+        # Get and remove the front of the queue
+        currNode = q[0]
+        q.popleft()
+        size = size - 1
+
+        # Get the current node's value from the string
+        currVal = ip[i]
+
+        # If the left child is not null
+        if (currVal != "N"):
+
+            # Create the left child for the current node
+            currNode.left = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.left)
+            size = size + 1
+        # For the right child
+        i = i + 1
+        if (i >= len(ip)):
+            break
+        currVal = ip[i]
+
+        # If the right child is not null
+        if (currVal != "N"):
+
+            # Create the right child for the current node
+            currNode.right = Node(int(currVal))
+
+            # Push it to the queue
+            q.append(currNode.right)
+            size = size + 1
+        i = i + 1
+    return root
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(0, t):
+        s = input()
+        root = buildTree(s)
+        obj = Solution()
+        res = obj.boundaryTraversal(root)
+        for i in res:
+            print(i, end=" ")
+        print('')
+        print("~")
+
+# } Driver Code Ends
