479b324b65e7761267acaea3da9b7c54717e857e
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-02-10 21:05:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.py
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.py
@@ -4,19 +4,28 @@ class Solution:
     def knapSack(self, val, wt,capacity):
         # code here
         n=len(val)
-        def knapsack(ind,tot):
-            if ind<0:
-                return 0
-            if dp[ind][tot]!=-1:
-                return dp[ind][tot]
-            l=0
-            if wt[ind]<=tot:
-                l=val[ind]+knapsack(ind,tot-wt[ind])
-            r=knapsack(ind-1,tot)
-            dp[ind][tot]=max(l,r)
-            return max(l,r)
-        dp=[[-1 for _ in range(capacity+1)] for _ in range(n)]
-        return knapsack(n-1,capacity)
+        dp=[[0 for i in range(capacity+1)] for _ in range(n+1)]
+        for ind in range(1,n+1):
+            for j in range(1,capacity+1):
+                l=0
+                if j>=wt[ind-1]:
+                    l=val[ind-1]+dp[ind][j-wt[ind-1]]
+                r=dp[ind-1][j]
+                dp[ind][j]=max(l,r)
+        return dp[n][capacity]
+        # def knapsack(ind,tot):
+        #     if ind<0:
+        #         return 0
+        #     if dp[ind][tot]!=-1:
+        #         return dp[ind][tot]
+        #     l=0
+        #     if wt[ind]<=tot:
+        #         l=val[ind]+knapsack(ind,tot-wt[ind])
+        #     r=knapsack(ind-1,tot)
+        #     dp[ind][tot]=max(l,r)
+        #     return max(l,r)
+        # dp=[[-1 for _ in range(capacity+1)] for _ in range(n)]
+        # return knapsack(n-1,capacity)
 
 #{ 
  # Driver Code Starts
