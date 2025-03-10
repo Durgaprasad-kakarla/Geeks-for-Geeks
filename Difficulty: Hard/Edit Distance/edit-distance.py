2505ad4fa9fd56dbdc61f53416fcb271b6a9bba1
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-03-10 20:45:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.py
+++ Difficulty: Hard/Edit Distance/edit-distance.py
@@ -21,9 +21,11 @@ class Solution:
 if __name__ == '__main__':
     T = int(input())
     for i in range(T):
-        s, t = input().split()
+        s1 = input()
+        s2 = input()
         ob = Solution()
-        ans = ob.editDistance(s, t)
+        ans = ob.editDistance(s1, s2)
         print(ans)
+        print("~")
 
 # } Driver Code Ends
