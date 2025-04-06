50e8ca1e5804a13c64c7e0b216ea50a115e7136a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-04-06 19:52:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.py
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.py
@@ -1,53 +1,49 @@
-#User function Template for python3
-from typing import List
-
 
 class Solution:
-    
-    #Function to detect cycle in a directed graph.
-    def isCyclic(self, V : int , adj : List[List[int]]) -> bool :
+    def isCycle(self, V, edges):
         # code here
-        def dfs(node,pathvis):
+        def dfs(node):
             vis[node]=1
             pathvis[node]=1
             for i in adj[node]:
                 if not vis[i]:
-                    if dfs(i,pathvis):
+                    if dfs(i):
                         return True
-                elif pathvis[i]:
+                elif pathvis[i]==1:
                     return True
             pathvis[node]=0
             return False
-        vis=[0 for i in range(V)]
-        pathvis=[0 for i in range(V)]
+        adj=[[] for i in range(V)]
+        vis,pathvis=[0 for i in range(V)],[0 for _ in range(V)]
+        for u,v in edges:
+            adj[u].append(v)
         for i in range(V):
-            if not vis[i]:
-                if dfs(i,pathvis):
-                    return True
+            if not vis[i] and dfs(i):
+                return True
         return False
         
 
 #{ 
  # Driver Code Starts
-#Initial Template for Python 3
+from collections import deque
 
-import sys
 
-sys.setrecursionlimit(10**6)
+def main():
+    tc = int(input())
+    for _ in range(tc):
+        V = int(input())
+        E = int(input())
+        edges = []
+        for _ in range(E):
+            u, v = map(int, input().split())
+            edges.append((u, v))
 
-if __name__ == '__main__':
-    t = int(input())
-    for i in range(t):
-        V, E = list(map(int, input().strip().split()))
-        adj = [[] for i in range(V)]
-        for i in range(E):
-            a, b = map(int, input().strip().split())
-            adj[a].append(b)
-        ob = Solution()
+        obj = Solution()
+        ans = obj.isCycle(V, edges)
+        print("true" if ans else "false")
 
-        if ob.isCyclic(V, adj):
-            print(1)
-        else:
-            print(0)
+
+if __name__ == "__main__":
+    main()
 
 # } Driver Code Ends
