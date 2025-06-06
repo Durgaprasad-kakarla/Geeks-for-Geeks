ee72faf3f4d748ff6e1ffaf36f83788b6aa559c8
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-06-06 08:40:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.py
+++ Difficulty: Medium/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.py
@@ -1,43 +1,27 @@
-#User function Template for python3
-
 class Solution:
-    def search(self, pattern, txt):
+    def search(self, pattern, text):
         # code here
-        q=(10**9+7)
-        n,m,h,d=len(txt),len(pattern),1,5
-        for i in range(1,m):
+        q,d=101,10
+        m=len(pattern)
+        n=len(text)
+        i,j,h=0,0,1
+        for i in range(m-1):
             h=(h*d)%q
-        t,p=0,0
+        p,t=0,0
         for i in range(m):
-            t=(t*d+(ord(txt[i])))%q
-            p=(p*d+(ord(pattern[i])))%q
+            p=(p*d+ord(pattern[i]))%q
+            t=(t*d+ord(text[i]))%q
         lst=[]
         for i in range(n-m+1):
-            if t==p:
-                flag=0
+            if p==t:
                 for j in range(m):
-                    if txt[i+j]!=pattern[j]:
-                        flag=1
+                    if text[i+j]!=pattern[j]:
                         break
-                if flag==0:
+                j+=1
+                if j==m:
                     lst.append(i+1)
             if i<n-m:
-                t=(d*(t-(ord(txt[i]))*h)+(ord(txt[i+m])))%q
+                t=(d*(t-ord(text[i])*h)+ord(text[i+m]))%q
                 if t<0:
                     t+=q
         return lst
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        s = input().strip()
-        patt = input().strip()
-        ob = Solution()
-        ans = ob.search(patt, s)
-        for value in ans:
-            print(value,end = ' ')
-        print()
-# } Driver Code Ends
