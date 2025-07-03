520919f7ba58288910abe7b7da85b676cf34c853
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-07-03 09:02:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.py
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.py
@@ -1,43 +1,21 @@
-#User function Template for python3
-
 class Solution:
-
     def longestKSubstr(self, s, k):
         # code here
-        maxi,n,start,f=-float('inf'),len(s),0,0
-        dic={}
+        n=len(s)
+        dic,start,cnt,maxi={},0,0,-float('inf')
         for i in range(n):
-            while start<n and f==k and (s[i] not in dic or dic[s[i]]==0):
-                dic[s[start]]-=1
-                if dic[s[start]]==0:
-                    f-=1
-                start+=1
-            if s[i] in dic and dic[s[i]]>0:
+            if s[i] in dic:
                 dic[s[i]]+=1
             else:
                 dic[s[i]]=1
-                f+=1
-            if f==k:
+                cnt+=1
+            while start<n and len(dic)>k:
+                dic[s[start]]-=1
+                if dic[s[start]]==0:
+                    del dic[s[start]]
+                start+=1
+            if len(dic)==k:
                 maxi=max(maxi,i-start+1)
         return maxi if maxi!=-float('inf') else -1
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-
-    t = int(input())
-
-    for _ in range(t):
-        s = input()
-        k = int(input())
-
-        solObj = Solution()
-
-        ans = solObj.longestKSubstr(s, k)
-
-        print(ans)
-
-        print("~")
-# } Driver Code Ends
+        
+        
