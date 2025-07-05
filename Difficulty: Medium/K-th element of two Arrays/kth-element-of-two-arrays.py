cd0c68879b288c29aacba4939d9948a74b7e140e
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-07-05 20:19:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
@@ -3,55 +3,28 @@
 
 class Solution:
 
-    def kthElement(self, arr, brr, k):
-        l,r,n,m=0,0,len(arr),len(brr)
-        while l<n and r<m:
-            if arr[l]<=brr[r]:
-                k-=1
-                if k==0:
-                    return arr[l]
-                l+=1
+    def kthElement(self, nums1,nums2, k):
+        n,m=len(nums1),len(nums2)
+        if n>m:
+            return self.kthElement(nums2,nums1,k)
+        l = max(0, k - m)
+        r = min(k,n)
+        while l<=r:
+            mid1=(l+r)//2
+            mid2=(k-mid1)
+            l1,l2,r1,r2=-float('inf'),-float('inf'),float('inf'),float('inf')
+            if mid1<n:
+                r1=nums1[mid1]
+            if mid2<m:
+                r2=nums2[mid2]
+            if mid1-1>=0:
+                l1=nums1[mid1-1]
+            if mid2-1>=0:
+                l2=nums2[mid2-1]
+            if l1>r2:
+                r=mid1-1
+            elif l2>r1:
+                l=mid1+1
             else:
-                k-=1
-                if k==0:
-                    return brr[r]
-                r+=1
-        while l<n:
-            k-=1
-            if k==0:
-                return arr[l]
-            l+=1
-        while r<m:
-            k-=1
-            if k==0:
-                return brr[r]
-            r+=1
-            
-         
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-
-def main():
-
-    T = int(input())
-
-    while (T > 0):
-
-        k = int(input())
-        a = [int(x) for x in input().strip().split()]
-        b = [int(x) for x in input().strip().split()]
-        ob = Solution()
-        print(ob.kthElement(a, b, k))
-        print("~")
-        T -= 1
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
+                return  max(l1,l2)
+        return -1
