efd1478d36ca24771e40037eca7bc6cd76ab12ce
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-07-26 11:09:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element II/majority-element-ii.py
+++ Difficulty: Medium/Majority Element II/majority-element-ii.py
@@ -1,19 +1,18 @@
 class Solution:
-    # Function to find the majority elements in the array
     def findMajority(self, arr):
-        #Your Code goes here.
+        # code here
         n=len(arr)
-        ele1,ele2,cnt1,cnt2=-1,-1,0,0
+        cnt1,cnt2,ele1,ele2=0,0,-1,-1
         for i in range(n):
             if cnt1==0 and ele2!=arr[i]:
                 ele1=arr[i]
                 cnt1+=1
             elif ele1==arr[i]:
                 cnt1+=1
+            elif ele2==arr[i]:
+                cnt2+=1
             elif cnt2==0:
                 ele2=arr[i]
-                cnt2+=1
-            elif ele2==arr[i]:
                 cnt2+=1
             else:
                 cnt1-=1
@@ -30,28 +29,3 @@ class Solution:
         if cnt2>n//3:
             lst.append(ele2)
         return sorted(lst)
-                
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-
-def main():
-    t = int(input().strip())
-    for _ in range(t):
-        s = input().strip()
-        nums = list(map(int, s.split()))
-        ob = Solution()
-        ans = ob.findMajority(nums)
-        if not ans:
-            print("[]")
-        else:
-            print(" ".join(map(str, ans)))
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
