b261e73e88ee997323967c97669b9c1b25d28cff
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-08-02 11:36:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
@@ -1,16 +1,9 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
-
-#User function Template for python3
 class Solution:
     def longestSubarray(self, arr, k):
-        # Code Here
+        # Code Here 
         n=len(arr)
-        maxi,pref=0,0
         dic={0:-1}
+        pref,maxi=0,0
         for i in range(n):
             if arr[i]>k:
                 pref+=1
@@ -20,24 +13,9 @@ class Solution:
                 maxi=max(maxi,i+1)
             else:
                 if pref-1 in dic:
-                    maxi=max(maxi,i-dic[pref-1])
+                    maxi=max(maxi,(i-dic[pref-1]))
             if pref not in dic:
                 dic[pref]=i
         return maxi
-
-
-#{ 
- # Driver Code Starts.
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        
-        arr = [int(x) for x in input().strip().split()]
-        k = int(input())
-        
-        ob = Solution()
-        print(ob.longestSubarray(arr, k))
-        print("~")
-        t -= 1
-# } Driver Code Ends
+            
+            
