d2849c5a7e25e9445457123b782de1e5ac9e2f24
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-08-08 08:37:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.py
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.py
@@ -1,35 +1,19 @@
-#User function Template for python3
-
 class Solution:
-	def lps(self, s):
+	def getLPSLength(self, s):
 		# code here
-        n=len(s)
-        i,l=1,0
+		n=len(s)
         lps=[0]*n
+        i,length=1,0
         while i<n:
-            if s[i]==s[l]:
-                lps[i]=l+1
-                l+=1
+            if s[i]==s[length]:
+                lps[i]=length+1
+                length+=1
                 i+=1
             else:
-                if l!=0:
-                    l=lps[l-1]
+                if length!=0:
+                    length=lps[length-1]
                 else:
                     lps[i]=0
                     i+=1
         return lps[-1]
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    T = int(input())
-    for i in range(T):
-        s = input()
-
-        ob = Solution()
-        answer = ob.lps(s)
-        print(answer)
-
-# } Driver Code Ends
+    		
