4027baebc7b521a4f8e1bc3182cf6cd85f287103
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-08-15 10:54:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Insert Interval/insert-interval.py
+++ Difficulty: Medium/Insert Interval/insert-interval.py
@@ -1,10 +1,3 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
-#User function Template for python3
-
 class Solution:
     def insertInterval(self, intervals, newInterval):
         # Code here
@@ -21,23 +14,3 @@ class Solution:
                 start,end=intervals[i]
         ans.append([start,end])
         return ans
-#{ 
- # Driver Code Starts.
-if __name__ == '__main__': 
-    t = int(input ())
-    for _ in range (t):
-        N = int(input())
-        intervals = [list(map(int, input().split())) for i in range(N)]
-        newEvent = list(map(int, input().split()))
-        ob = Solution()
-        res = ob.insertInterval(intervals, newEvent)
-        print('[', end = '')
-        for i in range(len(res)):
-            print('[', end = '')
-            print(str(res[i][0])+','+str(res[i][1]), end = '')
-            print(']', end = '')
-            if i < len(res)-1:
-                print(',', end='')
-        print(']')
-        print("~")
-# } Driver Code Ends
