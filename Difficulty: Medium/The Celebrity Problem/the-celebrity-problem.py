ed550ab53b673539cdceea83e79e4df89b43fd1d
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-08-30 12:35:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.py
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.py
@@ -1,35 +1,19 @@
 class Solution:
     def celebrity(self, mat):
         # code here
-        n=len(mat)
-        adj=[[] for _ in range(n)]
-        ind=-1
+        n = len(mat)
+        a, b = 0, n - 1
+        
+        while a < b:
+            if mat[a][b] == 1:  
+                a += 1
+            else:
+                b -= 1
+        
+        candidate = a
+        
         for i in range(n):
-            for j in range(n):
-                if mat[i][j]:
-                    adj[i].append(j)
-            if adj[i]==[]:
-                ind=i
-        if ind==-1:
-            return -1
-        for i in range(n):
-            if i!=ind and ind not in adj[i]:
+            if i != candidate:
+                if mat[candidate][i] or not mat[i][candidate]:
                     return -1
-        return ind
-
-
-#{ 
- # Driver Code Starts
-# Main function to handle input and output
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        n = int(input())
-        M = []
-        for _ in range(n):
-            M.append(list(map(int, input().split())))
-
-        ob = Solution()
-        print(ob.celebrity(M))
-
-# } Driver Code Ends
+        return candidate
