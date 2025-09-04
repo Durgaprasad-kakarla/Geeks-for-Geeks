fa25ba1b3fa5d085114b132af6b6101782f9654a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-09-04 09:46:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.py
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.py
@@ -1,102 +1,47 @@
-"""Node is defined as
-
+"""
 class Node:
     def __init__(self, data):
 		self.data = data
 		self.next = None
 """
+
 class Solution:
+    def reverse(self,head):
+        dummy,last=None,head
+        while head:
+            curr=head.next
+            head.next=dummy
+            dummy=head
+            head=curr
+        return dummy,last
+    def print_lst(self,head):
+        temp=head
+        while temp:
+            print(temp.data,end=" ")
+            temp=temp.next
+        print()
     def reverseKGroup(self, head, k):
         # Code here
-        def reverse(head):
-            prev = None
-            while head:
-                next_node = head.next
-                head.next = prev
-                prev = head
-                head = next_node
-            return prev
-        
-        def find_kth(head, k):
-            temp = head
-            while temp and k > 1:
-                temp = temp.next
-                k -= 1
-            return temp
-        temp = head
-        prev_group_tail = None
-        new_head = None
-        
+        temp=head
+        prev=None
+        cnt=0
         while temp:
-            kth_node = find_kth(temp, k)
-            if not kth_node:  # Less than k nodes in the current group
-                if prev_group_tail:
-                    prev_group_tail.next = reverse(temp)
+            cnt+=1
+            next_node=temp.next
+            if cnt%k==0 or not temp.next:
+                temp.next=None
+                if not prev:
+                    rev_head,last=self.reverse(head)
+                    head=rev_head
+                    last.next=next_node
+                    prev=last
                 else:
-                    new_head = reverse(temp)
-                break
-            
-            next_group_head = kth_node.next
-            kth_node.next = None  # Detach the current group
-            reversed_group = reverse(temp)
-            
-            if not new_head:
-                new_head = reversed_group
-            if prev_group_tail:
-                prev_group_tail.next = reversed_group
-            
-            prev_group_tail = temp  # `temp` becomes the tail of the reversed group
-            temp = next_group_head  # Move to the next group
-        
-        return new_head
-                    
-                
-            
-
-#{ 
- # Driver Code Starts
-class Node:
-
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-class LinkedList:
-
-    def __init__(self):
-        self.head = None
-
-    def push(self, new_data):
-        new_node = Node(new_data)
-        new_node.next = self.head
-        self.head = new_node
-
-    def printList(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" ")
-            temp = temp.next
-        print()
-
-
-if __name__ == '__main__':
-    t = int(input())  # Number of test cases
-    while t > 0:
-        llist = LinkedList()
-
-        # Read list values and push them to the LinkedList
-        values = list(map(int, input().split()))
-        for i in reversed(values):
-            llist.push(i)
-
-        k = int(input())  # Size of the group for reversal
-        ob = Solution()
-        new_head = ob.reverseKGroup(llist.head, k)
-        llist.head = new_head
-        llist.printList()  # Print the modified linked list
-        t -= 1
-
-        print("~")
-
-# } Driver Code Ends
+                    curr=prev.next
+                    prev.next=None
+                    rev_head,last=self.reverse(curr)
+                    prev.next=rev_head
+                    last.next=next_node
+                    prev=last
+                # self.print_lst(head)
+            temp=next_node
+        return head
