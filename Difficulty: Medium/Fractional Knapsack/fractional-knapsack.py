3a3c478c487122cdae2706ce5a373e72d1fb9754
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-09-14 13:45:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.py
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.py
@@ -1,43 +1,17 @@
-#User function Template for python3
 class Solution:
-    # Function to get the maximum total value in the knapsack.
-    def fractionalknapsack(self, val, wt, capacity):
+    def fractionalKnapsack(self, val, wt, capacity):
         #code here
-        n=len(val)
-        arr=[]
+        wt_val=[]
+        n=len(wt)
         for i in range(n):
-            arr.append([wt[i]/val[i],wt[i],val[i]])
-        arr.sort()
+            wt_val.append([wt[i]/val[i],wt[i],val[i]])
+        wt_val.sort()
         tot=0
         for i in range(n):
-            _,wt,val=arr[i][0],arr[i][1],arr[i][2]
-            if wt<=capacity:
-                tot+=val
-                capacity-=wt
+            if capacity>=wt_val[i][1]:
+                tot+=wt_val[i][2]
+                capacity-=wt_val[i][1]
             else:
-                tot+=(val/wt)*(capacity)
-                capacity=0
+                tot+=(capacity/wt_val[i][1])*wt_val[i][2]
+                break
         return tot
-
-
-#{ 
- # Driver Code Starts
-#Position this line where user code will be pasted.
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for _ in range(test_cases):
-        # Read values array
-        values = list(map(int, input().strip().split()))
-
-        # Read weights array
-        weights = list(map(int, input().strip().split()))
-
-        # Read the knapsack capacity
-        W = int(input().strip())
-
-        ob = Solution()
-        print("%.6f" % ob.fractionalknapsack(values, weights, W))
-        print("~")
-
-# } Driver Code Ends
