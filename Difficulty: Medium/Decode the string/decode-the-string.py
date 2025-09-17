15e5262f420c1f869ec24078794d398e8b12770e
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-09-17 07:32:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.py
+++ Difficulty: Medium/Decode the string/decode-the-string.py
@@ -1,41 +1,23 @@
-
 class Solution:
     def decodedString(self, s):
         # code here
+        n=len(s)
         stack=[]
-        i=len(s)-1
+        i=n-1
         while i>=0:
-            if s[i]=='[':
-                num=''
+            if stack and s[i].isdigit():
+                num=s[i]
                 i-=1
-                while i>=0 and s[i].isnumeric():
+                while i>=0 and s[i].isdigit():
                     num+=s[i]
                     i-=1
-                num=num[::-1]
+                stack.pop()
                 st=''
                 while stack and stack[-1]!=']':
                     st+=stack.pop()
                 stack.pop()
-                stack.append(int(num)*st)
+                stack.append(st*int(num[::-1]))
             else:
                 stack.append(s[i])
                 i-=1
         return "".join(stack[::-1])
-                
-            
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        s = input()
-
-        ob = Solution()
-        print(ob.decodedString(s))
-        print("~")
-
-# } Driver Code Ends
