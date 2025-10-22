7559b442ebede2cecdcb6f8d01f4fd70afc8540a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-10-22 10:21:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nearly sorted/nearly-sorted.py
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.py
@@ -1,37 +1,13 @@
-#User function Template for python3
-import heapq
+from heapq import heappush,heappop
 class Solution:
-    def nearlySorted(self, arr, k):
-        #code
-        n=len(arr)
+    def nearlySorted(self, arr, k):  
+        #code here
         heap=[]
-        for i in range(k+1):
-            heapq.heappush(heap,arr[i])
-        x=0
-        for i in range(k+1,n):
-            arr[x]=(heapq.heappop(heap))
-            heapq.heappush(heap,arr[i])
-            x+=1
+        n=len(arr)
+        for i in range(n):
+            heappush(heap,arr[i])
+        i=0
         while heap:
-            arr[x]=(heapq.heappop(heap))
-            x+=1
+            arr[i]=heappop(heap)
+            i+=1
         return arr
-            
-
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-# Position this line where user code will be pasted.
-# Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        arr = list(map(int, input().split()))
-        k = int(input())
-        ob = Solution()
-        ob.nearlySorted(arr, k)
-        print(*arr)
-        print("~")
-        t -= 1
-# } Driver Code Ends
