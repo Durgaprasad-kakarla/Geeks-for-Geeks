cf56a94e77d941a510e931c2f97d4b0b7a273f08
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-10-26 13:35:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.py
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.py
@@ -1,9 +1,8 @@
-
 class Solution:
-    def isCycle(self, V, edges):
+    def isCyclic(self, V, edges):
         # code here
         adj=[[] for _ in range(V)]
-        indegree=[0 for _ in range(V)]
+        indegree=[0]*(V)
         for u,v in edges:
             adj[u].append(v)
             indegree[v]+=1
@@ -19,30 +18,4 @@ class Solution:
                 indegree[i]-=1
                 if indegree[i]==0:
                     queue.append(i)
-        return not len(topo)==V
-        
-
-#{ 
- # Driver Code Starts
-from collections import deque
-
-
-def main():
-    tc = int(input())
-    for _ in range(tc):
-        V = int(input())
-        E = int(input())
-        edges = []
-        for _ in range(E):
-            u, v = map(int, input().split())
-            edges.append((u, v))
-
-        obj = Solution()
-        ans = obj.isCycle(V, edges)
-        print("true" if ans else "false")
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
+        return False if len(topo)==V else True
