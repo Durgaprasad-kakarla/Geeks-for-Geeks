821577629b1cac81d300a69d93c1625c27cc0083
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-10-26 13:18:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.py
+++ Difficulty: Medium/Topological sort/topological-sort.py
@@ -2,69 +2,18 @@ class Solution:
     
     def topoSort(self, V, edges):
         # Code here
+        adj=[[] for _ in range(V)]
+        for u,v in edges:
+            adj[u].append(v)
         def dfs(node):
             vis[node]=1
             for i in adj[node]:
                 if not vis[i]:
                     dfs(i)
-            topo.append(node)
-        vis=[0 for _ in range(V)]
-        adj=[[] for i in range(V)]
-        for u,v in edges:
-            adj[u].append(v)
-        topo=[]
+            stack.append(node)
+        vis=[0]*(V)
+        stack=[]
         for i in range(V):
             if not vis[i]:
                 dfs(i)
-        return topo[::-1]
-
-
-#{ 
- # Driver Code Starts
-# Driver Program
-
-import sys
-
-sys.setrecursionlimit(10**6)
-
-
-def check(graph, N, res):
-    if N != len(res):
-        return False
-    map = [0] * N
-    for i in range(N):
-        map[res[i]] = i
-    for i in range(N):
-        for v in graph[i]:
-            if map[i] > map[v]:
-                return False
-    return True
-
-
-def main():
-    tc = int(input())
-    for _ in range(tc):
-        V = int(input())
-        E = int(input())
-        x = V
-        edges = []
-        adj = [[] for i in range(V)]
-        for _ in range(E):
-            u, v = map(int, input().split())
-            edges.append((u, v))
-            adj[u].append(v)
-
-        obj = Solution()
-        res = obj.topoSort(V, edges)
-
-        if check(adj, x, res):
-            print("true")
-        else:
-            print("false")
-        print("~")
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
+        return stack[::-1]
