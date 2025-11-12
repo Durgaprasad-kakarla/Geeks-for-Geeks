c01f67e9c1411ccb2b90fcefa196735f26f54f2f
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-11-12 09:13:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.py
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.py
@@ -1,45 +1,14 @@
 #User function Template for python3
-
+import sys
 class Solution:
     def bellmanFord(self, V, edges, src):
-        #code here
         dist=[float('inf')]*V
         dist[src]=0
-        for i in range(V):
+        for i in range(V-1):
             for u,v,wt in edges:
                 if dist[v]>dist[u]+wt:
                     dist[v]=dist[u]+wt
         for u,v,wt in edges:
             if dist[v]>dist[u]+wt:
                 return [-1]
-        for i in range(V):
-            if dist[i]==float('inf'):
-                dist[i]=10**8
-        return dist
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import atexit
-import io
-import sys
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        V = int(input())
-        E = int(input())
-        edges = []
-        for i in range(E):
-            u, v, w = map(int, input().strip().split())
-            edges.append([u, v, w])
-        S = int(input())
-        ob = Solution()
-
-        res = ob.bellmanFord(V, edges, S)
-        for i in res:
-            print(i, end=" ")
-        print()
-        print("~")
-
-# } Driver Code Ends
+        return [dist[i] if dist[i]!=float('inf') else 10**8 for i in range(V)]
