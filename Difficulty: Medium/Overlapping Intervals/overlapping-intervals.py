c4a5a5a6081b79ff439bd045506b28a9d88aaeb8
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-12-17 14:59:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
@@ -1,39 +1,14 @@
 class Solution:
 	def mergeOverlap(self, arr):
-		#Code here
-		n=len(arr)
+		# Code here
 		arr.sort()
-		start,end=arr[0][0],arr[0][1]
-		ans=[]
+		n=len(arr)
+		ans=[arr[0]]
 		for i in range(1,n):
-            if arr[i][0]<=end:
-                end=max(end,arr[i][1])
-            else:
-                ans.append([start,end])
-                start,end=arr[i][0],arr[i][1]
-        ans.append([start,end])
-        return ans
-#{ 
- # Driver Code Starts
-if __name__ == '__main__':
-    T = int(input())
-    for i in range(T):
-        n = int(input())
-        # a = list(map(int, input().strip().split()))
-        arr = []
-        # j = 0
-        for i in range(n):
-            a = list(map(int, input().strip().split()))
-            x = a[0]
-            # j += 1
-            y = a[1]
-            # j += 1
-            arr.append([x, y])
-        obj = Solution()
-        ans = obj.mergeOverlap(arr)
-        for i in ans:
-            for j in i:
-                print(j, end=" ")
-        print()
-
-# } Driver Code Ends
+		    start,end=arr[i]
+		    if ans[-1][1]>=start:
+		        ans[-1][1]=max(ans[-1][1],end)
+		        ans[-1][0]=min(ans[-1][0],start)
+		    else:
+		        ans.append(arr[i])
+		return ans
