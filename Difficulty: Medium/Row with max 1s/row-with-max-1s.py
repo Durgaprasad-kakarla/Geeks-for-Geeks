596c00b8c7812880259abd097f8afa9b8d8fb70a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-12-22 10:49:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.py
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.py
@@ -1,46 +1,31 @@
-#User function Template for python3
 class Solution:
-
-	def rowWithMax1s(self,arr):
-		# code here
-		def binary_search(arr):
-		    n=len(arr)
-		    l,r=0,n-1
-		    while l<=r:
-		        mid=(l+r)//2
-		        if arr[mid]==1:
-		            r=mid-1
-		        else:
-		            l=mid+1
-		    return (n-(r+1))
-		maxi=0
-		ind=-1
-		for i in range(len(arr)):
-		    cnt_1=binary_search(arr[i])
-		    if maxi<cnt_1:
-		        maxi=cnt_1
-		        ind=i
-		return ind
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    tc = int(input())
-    while tc > 0:
-        n, m = list(map(int, input().strip().split()))
-
-        inputLine = list(map(int, input().strip().split()))
-        arr = [[0 for j in range(m)] for i in range(n)]
-
+    def rowWithMax1s(self, arr):
+        # code here
+        def no_of_ones(arr):
+            n=len(arr)
+            l,r=0,n-1
+            while l<=r:
+                mid=(l+r)//2
+                if arr[mid]<=1:
+                    l=mid+1
+                else:
+                    r=mid-1
+            upper_bound=l
+            l,r=0,n-1
+            while l<=r:
+                mid=(l+r)//2
+                if arr[mid]<1:
+                    l=mid+1
+                else:
+                    r=mid-1
+            lower_bound=l
+            return (upper_bound-lower_bound)
+        maxi=-float('inf')
+        n=len(arr)
         for i in range(n):
-            for j in range(m):
-                arr[i][j] = inputLine[i * m + j]
-        ob = Solution()
-        ans = ob.rowWithMax1s(arr)
-        print(ans)
-        tc -= 1
-
-# } Driver Code Ends
+            if maxi<no_of_ones(arr[i]):
+               maxi=no_of_ones(arr[i])
+               max_ind=i
+        return -1 if maxi==-float('inf') else max_ind
+            
+            
