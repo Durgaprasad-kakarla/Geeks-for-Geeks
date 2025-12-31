4bc899dadbc33e0bf432c814a731d2d41e2a289f
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2025-12-31 13:04:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.py
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.py
@@ -1,91 +1,36 @@
-#User function Template for python3
 '''
-	Your task is to check if given linkedlist
-	is a pallindrome or not.
-	
-	Function Arguments: head (reference to head of the linked list)
-	Return Type: boolean , no need to print just return True or False.
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-	{
-		# Node Class
-		class Node:
-		    def __init__(self, data):   # data -> value stored in node
-		        self.data = data
-		        self.next = None
-	}
-
-	Contributed By: Nagendra Jha
 '''
-#Function to check whether the list is palindrome.
+
 class Solution:
     def isPalindrome(self, head):
-        #code here
+        # code here
+        def reverse(head):
+            dummy=None
+            while head:
+                curr=head.next
+                head.next=dummy
+                dummy=head
+                head=curr
+            return dummy
         slow,fast,prev=head,head,head
         while fast and fast.next:
             prev=slow
             slow=slow.next
             fast=fast.next.next
+        middle=prev
+        head2=prev.next
         prev.next=None
-        new_head=slow
-        dummy=None
-        while new_head:
-            curr=new_head.next
-            new_head.next=dummy
-            dummy=new_head
-            new_head=curr
-        while head and dummy:
-            if head.data!=dummy.data:
-                return False 
+        head2=reverse(head2)
+        while head and head2:
+            if head.data!=head2.data:
+                return False
             head=head.next
-            dummy=dummy.next
+            head2=head2.next
         return True
-
-
-#{ 
- # Driver Code Starts
-#main
-
-
-class Node:
-    # Constructor to initialize the node object
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-class LinkedList:
-    # Function to initialize head
-    def __init__(self):
-        self.head = None
-
-    # Function to insert a new node at the beginning
-    def push(self, new_data):
-        new_node = Node(new_data)
-        new_node.next = self.head
-        self.head = new_node
-
-    # Utility function to prit the linked LinkedList
-    def printList(self):
-        temp = self.head
-        while (temp):
-            print(temp.data, end=" ")
-            temp = temp.next
-        print("")
-
-
-if __name__ == '__main__':
-    t = int(input())
-    while (t > 0):
-        llist = LinkedList()
-        values = input().strip().split()
-        for i in reversed(values):
-            llist.push(i)
-        flag = Solution().isPalindrome(llist.head)
-        if flag:
-            print("true")
-        else:
-            print("false")
-        t -= 1
-# Contributed by: Harshit Sidhwa
-
-# } Driver Code Ends
+        
+        
