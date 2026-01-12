e329b94b73120a24e4845175e186b07cd53a76b1
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-01-12 16:31:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.py
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.py
@@ -1,50 +1,16 @@
 class Solution:
     def maxOfSubarrays(self, arr, k):
         # code here
-        queue=deque()
         n=len(arr)
-        ans=[]
+        queue=deque()
+        window_max=[]
         for i in range(n):
-            while queue and queue[-1][0]<=arr[i]:
+            while queue and queue[0][1]<=(i-k):
+                queue.popleft()
+            while queue and queue[-1][0]<arr[i]:
                 queue.pop()
             queue.append([arr[i],i])
-            while queue and i-queue[0][1]>=k:
-                queue.popleft()
-            if i-k+1>=0:
-                ans.append(queue[0][0])
-        return ans
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-import atexit
-import io
-import sys
-from collections import deque
-
-#Contributed by : Nagendra Jha
-
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
-
-
-@atexit.register
-def write():
-    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
-
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        arr = list(map(int, input().strip().split()))
-        k = int(input())
-        ob = Solution()
-        res = ob.maxOfSubarrays(arr, k)
-        for i in range(len(res)):
-            print(res[i], end=" ")
-        print()
-        print("~")
-# } Driver Code Ends
+            if i>=k-1:
+                window_max.append(queue[0][0])
+        return window_max
+            
