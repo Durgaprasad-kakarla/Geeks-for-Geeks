841c36ef117bdc0156dc5d558087274156aff0fe
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-01-14 13:03:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.py
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.py
@@ -1,35 +1,28 @@
 from collections import deque
 class Solution:
     def catchThieves(self, arr, k):
-        #code  here
-        stack=[]
+        #code here
+        theif_queue,police_queue=deque(),deque()
         n=len(arr)
         cnt=0
-        queue,stack=deque(),[]
-        for i in range(n-1,-1,-1):
-            if arr[i]=='T':
-                stack.append(i)
         for i in range(n):
+            # print('prev',police_queue,theif_queue)
+            while theif_queue and theif_queue[0]<(i-k):
+                theif_queue.popleft()
+            while police_queue and police_queue[0]<(i-k):
+                police_queue.popleft()
+            # print(police_queue,theif_queue)
             if arr[i]=='P':
-                flag=0
-                while queue and (i-queue[0]>k or (i-queue[0]<=k and arr[queue[0]]==-1)):
-                    queue.popleft()
-                if queue and i-queue[0]<=k:
-                    arr[queue[0]]=-1
+                if theif_queue:
+                    theif_queue.popleft()
                     cnt+=1
-                    flag=1
-                    queue.popleft()
-                if flag==0:
-                    while stack and stack[-1]<=i:
-                        stack.pop()
-                    if stack and stack[-1]-i<=k:
-                        arr[stack[-1]]=-1
-                        cnt+=1
-                        stack.pop()
+                else:
+                    police_queue.append(i)
             else:
-                queue.append(i)
-        return cnt
+                if police_queue:
+                    police_queue.popleft()
+                    cnt+=1
+                else:
+                    theif_queue.append(i)
             
-                    
-                
-                    
+        return cnt
