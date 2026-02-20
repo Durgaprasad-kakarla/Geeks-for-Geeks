60802ed0df884dd62f6c2faa0a96f732ffdc76e4
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-02-20 17:56:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form the Largest Number/form-the-largest-number.py
+++ Difficulty: Medium/Form the Largest Number/form-the-largest-number.py
@@ -1,14 +1,14 @@
-from functools import cmp_to_key
 class Solution:
+
 	def findLargest(self, arr):
-	    
+	    # code here
 	    def comp(x,y):
-	        if x+y>y+x:
-	            return 1
-	        elif x==y:
-	            return 0
-	        return -1
-	    arr=[str(i) for i in arr]
-	    arr.sort(key=cmp_to_key(comp),reverse=True)
-	    st="".join(arr).lstrip('0')
-	    return st if st!='' else '0'
+            if x+y>y+x:
+                return 1
+            elif x==y:
+                return 0
+            return -1
+        arr=[str(i) for i in arr]
+        arr.sort(key=cmp_to_key(comp),reverse=True)
+        st="".join(arr).lstrip('0')
+        return st if st!='' else '0'
