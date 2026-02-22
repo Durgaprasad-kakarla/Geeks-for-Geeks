7f361ec03e78e841b52b93431015a05eb6455c85
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-02-22 17:10:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.py
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.py
@@ -2,30 +2,13 @@ class Solution:
     def subarrayXor(self, arr, k):
         # code here
         dic={0:1}
-        pref=cnt=0
-        n=len(arr)
+        n,xor,tot=len(arr),0,0
         for i in range(n):
-            pref^=arr[i]
-            if pref^k in dic:
-                cnt+=dic[pref^k]
-            if pref in dic:
-                dic[pref]+=1
+            xor^=arr[i]
+            if xor^k in dic:
+                tot+=dic[xor^k]
+            if xor in dic:
+                dic[xor]+=1
             else:
-                dic[pref]=1
-        return cnt
-
-
-#{ 
- # Driver Code Starts
-if __name__ == "__main__":
-    tc = int(input())
-
-    for _ in range(tc):
-        arr = list(map(int, input().split()))
-        k = int(input())
-
-        obj = Solution()
-        print(obj.subarrayXor(arr, k))
-        print("~")
-
-# } Driver Code Ends
+                dic[xor]=1
+        return tot
