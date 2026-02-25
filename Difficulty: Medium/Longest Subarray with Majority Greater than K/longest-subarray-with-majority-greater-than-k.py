1d9a0607b252c42909751d841061de732ffe2657
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-02-25 12:12:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
@@ -1,21 +1,19 @@
 class Solution:
     def longestSubarray(self, arr, k):
-        # Code Here 
+        # Code Here
         n=len(arr)
         dic={0:-1}
         pref,maxi=0,0
         for i in range(n):
-            if arr[i]>k:
-                pref+=1
-            else:
+            if arr[i]<=k:
                 pref-=1
+            else:
+                pref+=1
             if pref>0:
                 maxi=max(maxi,i+1)
             else:
                 if pref-1 in dic:
-                    maxi=max(maxi,(i-dic[pref-1]))
+                    maxi=max(maxi,i-dic[pref-1])
             if pref not in dic:
                 dic[pref]=i
         return maxi
-            
-            
