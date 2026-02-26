d38782e775eabbdce26ba24c244f7e141124cb51
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-02-26 13:15:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
@@ -1,32 +1,18 @@
 class Solution:
     def matrixMultiplication(self, arr):
         # code here
-        def multi(i,j):
-            if i==j:
-                return 0
-            mini=float("inf")
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            for k in range(i,j):
-                ans=multi(i,k)+multi(k+1,j)+arr[i-1]*arr[k]*arr[j]
-                mini=min(mini,ans)
-            dp[i][j]=mini
-            return mini
         n=len(arr)
-        dp=[[-1 for _ in range(n)] for _ in range(n)]
-        return multi(1,n-1)
+        dp=[[float('inf') for _ in range(n+1)] for _ in range(n+1)]
+        for i in range(n):
+            dp[i][i]=0
+        for d in range(1,n-1):
+            for i in range(1,n):
+                j=i+d
+                if j>=n:
+                    break
+                mini=float('inf')
+                for k in range(i,j):
+                    mini=min(mini,arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j])
+                dp[i][j]=mini
+        return dp[1][n-1]
         
-
-
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-
-t = int(input())  # number of test cases
-for _ in range(t):
-    arr = list(map(int, input().split()))  # input array
-    s = Solution().matrixMultiplication(arr)  # find the missing number
-    print(s)  # print the result
-    print("~")
-
-# } Driver Code Ends
