51724297abcb88c9a1e94bc27abc19824b7de982
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-03-01 19:22:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.py
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.py
@@ -1,35 +1,17 @@
-#User function Template for python3
-
 class Solution:
-	def pushZerosToEnd(self,arr):
+	def pushZerosToEnd(self, arr):
     	# code here
     	n=len(arr)
     	l,r=0,0
     	while r<n:
-    	    if arr[l]==0:
-    	        if arr[r]==0:
-    	            r+=1
-    	        else:
-    	            arr[l],arr[r]=arr[r],arr[l]
-    	            r+=1
+    	    if arr[l]>0:
+    	        if arr[r]>0:
     	            l+=1
+    	        r+=1
     	    else:
-	            l+=1
-	            r+=1
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    tc = int(input())
-    while tc > 0:
-        arr = list(map(int, input().strip().split()))
-        ob = Solution()
-        ob.pushZerosToEnd(arr)
-        for x in arr:
-            print(x, end=" ")
-        print()
-        tc -= 1
-# } Driver Code Ends
+    	        if arr[r]>0:
+        	        arr[l],arr[r]=arr[r],arr[l]
+        	        l+=1
+        	    r+=1
+    	return arr
+    	        
