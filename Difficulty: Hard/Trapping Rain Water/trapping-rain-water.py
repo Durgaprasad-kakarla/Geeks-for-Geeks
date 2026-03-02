4d8f88f252c04f172233c498a6e5146533db0d2c
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-03-02 15:42:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.py
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.py
@@ -1,38 +1,14 @@
-
 class Solution:
     def maxWater(self, arr):
         # code here
         n=len(arr)
-        suff_max=[-1]*n
-        suff_max[-1],pref,tot=arr[-1],arr[0],0
-        for i in range(n-2,-1,-1):
-            suff_max[i]=max(suff_max[i+1],arr[i])
-        for i in range(1,n-1):
-            pref=max(pref,arr[i])
-            mini=min(suff_max[i],pref)
-            tot+=(mini-arr[i])
+        pref=[0]*n
+        maxi=0
+        for i in range(n):
+            pref[i]=max(maxi,arr[i])
+            maxi=pref[i]
+        tot=curr_max=0
+        for i in range(n-1,-1,-1):
+            curr_max=max(curr_max,arr[i])
+            tot+=min(curr_max,pref[i])-arr[i]
         return tot
-
-#{ 
- # Driver Code Starts
-#Initial template for Python 3
-
-import math
-
-
-def main():
-    t = int(input())
-    while (t > 0):
-
-        arr = [int(x) for x in input().strip().split()]
-        obj = Solution()
-        print(obj.maxWater(arr))
-
-        t -= 1
-        print("~")
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
