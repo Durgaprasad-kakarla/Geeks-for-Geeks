36c293ebfa40bd87311562e895abbfbbafb80a6a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-03-08 18:23:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.py
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.py
@@ -1,12 +1,11 @@
 class Solution:
 	def pythagoreanTriplet(self, arr):
     	# code here
-        arr=list(set(arr))
-        n=len(arr)
-        for i in range(n):
-            arr[i]=arr[i]*arr[i]
-        for i in range(n):
-            for j in range(i-1,-1,-1):
-                if arr[i]+arr[j] in arr:
-                    return True
-        return False
+    	arr=list(set(arr))
+    	arr=[i*i for i in arr]
+    	n=len(arr)
+    	for i in range(n):
+    	    for j in range(i+1,n):
+    	        if arr[i]+arr[j] in arr:
+    	            return True
+    	return False
