4aa739e1ac23650e71add0102a47115ebaf62e71
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-03-19 15:45:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest BST/largest-bst.py
+++ Difficulty: Medium/Largest BST/largest-bst.py
@@ -1,95 +1,23 @@
-#User function Template for python3
 class Solution:
     def largestBst(self, root):
         # Your code here
-        def postorder(root):
-            if not root:
-                return float('inf'), -float('inf'), 0
-            min1, max1, cnt1 = postorder(root.left)
-            min2, max2, cnt2 = postorder(root.right)
-            if max1 < root.data < min2:
-                return min(root.data, min1), max(root.data, max2), cnt1 + cnt2 + 1
-            return -float('inf'), float('inf'), max(cnt1, cnt2)
-        _, _, cnt = postorder(root)
-        return cnt
-#{ 
- # Driver Code Starts
-from collections import deque
-
-
-# Tree Node
-class Node:
-
-    def __init__(self, val):
-        self.right = None
-        self.data = val
-        self.left = None
-
-
-# Function to Build Tree
-def buildTree(s):
-    # Corner Case
-    if len(s) == 0 or s[0] == "N":
-        return None
-
-    # Creating list of strings from input string after splitting by space
-    ip = list(map(str, s.split()))
-
-    # Create the root of the tree
-    root = Node(int(ip[0]))
-    size = 0
-    q = deque()
-
-    # Push the root to the queue
-    q.append(root)
-    size += 1
-
-    # Starting from the second element
-    i = 1
-    while size > 0 and i < len(ip):
-        # Get and remove the front of the queue
-        currNode = q.popleft()
-        size -= 1
-
-        # Get the current node's value from the string
-        currVal = ip[i]
-
-        # If the left child is not null
-        if currVal != "N":
-            # Create the left child for the current node
-            currNode.left = Node(int(currVal))
-            # Push it to the queue
-            q.append(currNode.left)
-            size += 1
-
-        # Move to the next element
-        i += 1
-        if i >= len(ip):
-            break
-
-        currVal = ip[i]
-
-        # If the right child is not null
-        if currVal != "N":
-            # Create the right child for the current node
-            currNode.right = Node(int(currVal))
-            # Push it to the queue
-            q.append(currNode.right)
-            size += 1
-
-        # Move to the next element
-        i += 1
-
-    return root
-
-
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        s = input()
-        root = buildTree(s)
-        ob = Solution()
-        result = ob.largestBst(root)
-        print(result)
-
-# } Driver Code Ends
+        def largest_bst(root):
+            global maxi
+            if root is None:
+                return (float('inf'),-float('inf'),0,'Y')
+            if not root.left and not root.right:
+                return (root.data,root.data,1,'Y')
+            min_left,max_left,cnt_left,flag_left=largest_bst(root.left)
+            min_right,max_right,cnt_right,flag_right=largest_bst(root.right)
+            # print(root.data,max_left,min_left,max_right,min_right,cnt_left,cnt_right)
+            if root.data>max_left and root.data<min_right and flag_left=='Y' and flag_right=='Y':
+                maxi=max(maxi,cnt_left+cnt_right+1)
+                # print(maxi)
+                return (min(root.data,min_left),max(root.data,max_right),cnt_left+cnt_right+1,'Y')
+            return (root.data,root.data,0,'N')
+            
+        global maxi
+        maxi=1
+        largest_bst(root)
+        return maxi
+        
