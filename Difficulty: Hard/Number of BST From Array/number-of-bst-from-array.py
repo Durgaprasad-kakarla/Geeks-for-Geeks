2f858208545a79f9828692327c2099041bf3fe92
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-03-21 17:56:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.py
+++ Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.py
@@ -1,21 +1,22 @@
-from functools import lru_cache
-from bisect import bisect_left
-        
 class Solution:
     def countBSTs(self, arr):
         # Code here
-        @lru_cache(None)
-        def count(n):
-            """given a number of node, how many bst can be formed"""
-            if n == 0 or n == 1:
-                return 1
-            return sum(count(i)*count(n-1-i) for i in range(n))
-            
-        sarr = list(sorted(arr))
+        n = len(arr)
         
-        ans = []
-        for e in arr:
-            left = bisect_left(sarr, e)
-            right = len(sarr) - 1 - left
-            ans.append(count(left)*count(right))
-        return ans
+        catalan = [0] * 7
+        catalan[0] = catalan[1] = 1
+        
+        for i in range(2, 7):
+            for j in range(i):
+                catalan[i] += catalan[j] * catalan[i - j - 1]
+        
+        result = []
+        
+        for i in range(n):
+            root = arr[i]
+            left = sum(1 for x in arr if x < root)
+            right = sum(1 for x in arr if x > root)
+            
+            result.append(catalan[left] * catalan[right])
+        
+        return result
