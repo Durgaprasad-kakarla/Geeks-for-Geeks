2771251e167ee1b9fad980cd64dd303be6a68fd9
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-03-27 15:30:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Chocolates Pickup/chocolates-pickup.py
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.py
@@ -1,43 +1,30 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
-#User function Template for python3
-
 class Solution:
-    def solve(self, n, m, grid):
-        # Code here
-        def func(r,c1,c2,dp):
-            if c1<0 or c2<0 or c1==m or c2==m:
-                return 0
-            if r==n:
-                return 0
-            if dp[r][c1][c2]!=-1:
-                return dp[r][c1][c2]
-            maxi=0
-            dc=[-1,0,1]
-            for i in range(3):
-                for j in range(3):
-                    maxi=max(maxi,func(r+1,c1+dc[i],c2+dc[j],dp))
-            if c1==c2:
-                dp[r][c1][c2]=maxi+grid[r][c1]
-                return maxi+grid[r][c1]
-            dp[r][c1][c2]=maxi+grid[r][c1]+grid[r][c2]
-            return maxi+grid[r][c1]+grid[r][c2]
-        dp=[[[-1 for i in range(m)] for j in range(m)] for k in range(n)]
-        return func(0,0,m-1,dp)
-            
-
-#{ 
- # Driver Code Starts.
-if __name__ == '__main__': 
-    t = int(input ())
-    for _ in range (t):
-        n, m = list(map(int, input().split()))
-        grid = [list(map(int, input().split())) for _ in range(n)]
-        obj = Solution()
-        res = obj.solve(n, m, grid)
-        print(res)
-        print("~")
-# } Driver Code Ends
+    def maxChocolate(self, grid):
+        # code here
+        def check(row,col):
+            if row<0 or row>=n or col<0 or col>=m:
+                return True
+            return False
+        def chocolate_pickup(i1,j1,i2,j2):
+            if check(i1,j1) or check(i2,j2):
+                return -float('inf')
+            if i1==n-1 and i2==n-1:
+                if j1==j2:
+                    return grid[i1][j1]
+                return grid[i2][j2]+grid[i1][j1]
+            if dp[i1][j1][i2][j2]!=-1:
+                return dp[i1][j1][i2][j2]
+            if i1==i2 and j1==j2:
+                sm=grid[i1][j1]
+            else:
+                sm=grid[i1][j1]+grid[i2][j2]
+            maxi=-float('inf')
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    maxi=max(maxi,
+                            chocolate_pickup(i1+1,j1+i,i2+1,j2+j))
+            dp[i1][j1][i2][j2]=maxi+sm
+            return dp[i1][j1][i2][j2]
+        n,m=len(grid),len(grid[0])
+        dp=[[[[-1 for _ in range(m)] for _ in range(n)] for _ in range(m)] for _ in range(n)]
+        return chocolate_pickup(0,0,0,m-1)
