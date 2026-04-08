285530b99707cf9952945ab0e47ebcd22d95dff4
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-04-08 11:45:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.py
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.py
@@ -1,44 +1,17 @@
-#User function Template for python3
-
 class Solution:
     def segregate0and1(self, arr):
         # code here
-        l,r,n=0,0,len(arr)
+        n=len(arr)
+        l,r=0,0
         while r<n:
-            if arr[r]==0:
-                arr[l],arr[r]=arr[r],arr[l]
+            if arr[l]==0:
                 l+=1
                 r+=1
             else:
-                r+=1
-
-
-#{ 
- # Driver Code Starts
-class IntArray:
-
-    def __init__(self) -> None:
-        pass
-
-    def Input(self, n):
-        arr = [int(i) for i in input().strip().split()]  #array input
+                if arr[r]==1:
+                    r+=1
+                else:
+                    arr[l],arr[r]=arr[r],arr[l]
+                    l+=1
+                    r+=1
         return arr
-
-    def Print(self, arr):
-        for i in arr:
-            print(i, end=" ")
-        print()
-
-
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-
-        arr = list(map(int, input().strip().split()))
-
-        obj = Solution()
-        obj.segregate0and1(arr)
-
-        print(*arr)
-
-# } Driver Code Ends
