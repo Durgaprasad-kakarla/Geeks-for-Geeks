fcd3535a2fc94a65078b6f7de1173bfc7d622d14
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-05-15 19:14:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Special Keyboard/special-keyboard.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def optimalKeys(self, n: int) -> int:
+        # code here
+        if n <= 6:
+            return n
+
+        dp = [0] * (n + 1)
+
+        # Base cases
+        for i in range(1, 7):
+            dp[i] = i
+
+        for i in range(7, n + 1):
+            for j in range(i - 3, 0, -1):
+
+                curr = dp[j] * (i - j - 1)
+
+                dp[i] = max(dp[i], curr)
+
+        return dp[n]
