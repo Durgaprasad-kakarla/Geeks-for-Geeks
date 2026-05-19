b35e0c51c6a812e3ded926bf27b8c4a9a1c8b6e6
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-05-19 10:15:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.py
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.py
@@ -1,20 +1,21 @@
 import heapq
 class Solution:
-    def minimumMultiplications(self, arr : list[int], start : int, end : int) -> int:
-        # code here
-        n=len(arr)
-        mod=100000
+    def minSteps(self, arr, start, end):
+        # code here2
         heap=[]
-        heapq.heappush(heap,[0,start])
-        dist=[float('inf')]*100001
+        mod=1000
+        heapq.heappush(heap,[0,-start])
+        dist=[float('inf')]*1000
         dist[start]=0
         while heap:
             d,node=heapq.heappop(heap)
+            node*=-1
+            node%=mod
             if node==end:
                 return d
             for i in arr:
                 curr=(node*i)%mod
                 if dist[curr]>d+1:
                     dist[curr]=d+1
-                    heapq.heappush(heap,[d+1,curr%mod])
+                    heapq.heappush(heap,[d+1,-curr])
         return -1
