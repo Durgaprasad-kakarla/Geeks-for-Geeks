e5e8975fa85f647c7fadd49ca3d0562b1fc3b16a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-07-19 22:51:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.py
+++ Difficulty: Medium/Frog Jump/frog-jump.py
@@ -1,20 +1,17 @@
 class Solution:
     def minCost(self, height):
         # code here
-        n=len(height)
-        def func(ind,arr):
-            if ind<0:
-                return float('inf')
+        def min_cost(ind,dp):
             if ind==0:
                 return 0
             if dp[ind]!=-1:
                 return dp[ind]
-            l,r=float('inf'),float('inf')
-            if ind>0:
-                l=abs(arr[ind]-arr[ind-1])+func(ind-1,arr)
+            l=abs(arr[ind]-arr[ind-1])+min_cost(ind-1,dp)
+            r=float('inf')
             if ind>1:
-                r=abs(arr[ind]-arr[ind-2])+func(ind-2,arr)
+                r=abs(arr[ind]-arr[ind-2])+min_cost(ind-2,dp)
             dp[ind]=min(l,r)
-            return min(l,r)
-        dp=[-1]*n
-        return func(n-1,arr)
+            return dp[ind]
+        n=len(height)
+        dp=[-1 for _ in range(n)]
+        return min_cost(n-1,dp)
