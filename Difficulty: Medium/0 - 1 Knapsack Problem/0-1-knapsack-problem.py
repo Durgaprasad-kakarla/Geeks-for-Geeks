e1f6cca04612ee3f8208103976ca8cc81f409e4b
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-07-29 20:46:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -1,30 +1,32 @@
 class Solution:
     def knapsack(self, W, val, wt):
         # code here
-        def func(ind,W,dp):
-            if ind<0:
-                return 0
-            if dp[ind][W]!=-1:
-                return dp[ind][W]
-            l=-float('inf')
-            if W>=wt[ind]:
-                l=val[ind]+func(ind-1,W-wt[ind],dp)
-            r=func(ind-1,W,dp)
-            dp[ind][W]=max(l,r)
-            return max(l,r) 
-        dp=[[-1 for i in range(W+1)] for j in range(len(wt))]
-        return func(len(wt)-1,W,dp)
-
-
-#{ 
- # Driver Code Starts
-if __name__ == '__main__':
-    test_cases = int(input())
-    for _ in range(test_cases):
-        capacity = int(input())
-        values = list(map(int, input().strip().split()))
-        weights = list(map(int, input().strip().split()))
-        ob = Solution()
-        print(ob.knapsack(capacity, values, weights))
-        print("~")
-# } Driver Code Ends
+        # n=len(val)
+        # def knapsack(ind,W):
+        #     if ind==0:
+        #         if W>=wt[ind]:
+        #             return val[ind]
+        #         return 0
+        #     if dp[ind][W]!=-1:
+        #         return dp[ind][W]
+        #     l=knapsack(ind-1,W)
+        #     r=-float('inf')
+        #     if W>=wt[ind]:
+        #         r=val[ind]+knapsack(ind-1,W-wt[ind])
+        #     dp[ind][W]=max(l,r)
+        #     return max(l,r)
+        # dp=[[-1 for _ in range(W+1)] for _ in range(n)]
+        # return knapsack(n-1,W)
+        n=len(val)
+        dp=[[0 for _ in range(W+1)] for _ in range(n)]
+        for i in range(W+1):
+            if i>=wt[0]:
+                dp[0][i]=val[0]
+        for i in range(1,n):
+            for j in range(W+1):
+                l=dp[i-1][j]
+                r=-float('inf')
+                if j>=wt[i]:
+                    r=val[i]+dp[i-1][j-wt[i]]
+                dp[i][j]=max(l,r)
+        return dp[n-1][W]
