475d3d1483b03aa4e221e39ac3f8d8b34ad8953a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-07-31 12:52:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
@@ -2,17 +2,16 @@ class Solution:
     def matrixMultiplication(self, arr):
         # code here
         n=len(arr)
-        dp=[[float('inf') for _ in range(n+1)] for _ in range(n+1)]
-        for i in range(n):
-            dp[i][i]=0
-        for d in range(1,n-1):
-            for i in range(1,n):
-                j=i+d
-                if j>=n:
-                    break
-                mini=float('inf')
-                for k in range(i,j):
-                    mini=min(mini,arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j])
-                dp[i][j]=mini
-        return dp[1][n-1]
+        def matrix_chain(i,j):
+            if i==j:
+                return 0
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            mini=float('inf')
+            for k in range(i,j):
+                mini=min(mini,(arr[i-1]*arr[k]*arr[j])+matrix_chain(i,k)+matrix_chain(k+1,j))
+            dp[i][j]=mini
+            return mini
+        dp=[[-1 for _ in range(n)] for _ in range(n)]
+        return matrix_chain(1,n-1)
         
