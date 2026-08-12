408dfcf1665e52e1cac152499dbf2a7fc9e52548
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-08-12 14:50:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.py
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.py
@@ -1,32 +1,13 @@
-# User function Template for python3
-
 class Solution:
     def longestSubarray(self, arr, k):  
         # code here
         n=len(arr)
-        maxi,dic,pref=0,{0:-1},0
+        dic={0:-1}
+        sm,maxi=0,0
         for i in range(n):
-            pref+=arr[i]
-            if pref-k in dic:
-                maxi=max(maxi,i-dic[pref-k])
-            if pref not in dic:
-                dic[pref]=i
+            sm+=arr[i]
+            if sm-k in dic:
+                maxi=max(maxi,i-dic[sm-k])
+            if sm not in dic:
+                dic[sm]=i
         return maxi
-    
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    tc = int(input().strip())
-    while tc > 0:
-        arr = list(map(int, input().strip().split()))
-        k = int(input().strip())
-        ob = Solution()
-        print(ob.longestSubarray(arr, k))
-        tc -= 1
-        print("~")
-# } Driver Code Ends
