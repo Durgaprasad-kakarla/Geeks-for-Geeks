64381af369b963f5615e5bb758245102f2d7662a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-08-17 22:18:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.py
+++ Difficulty: Medium/Count Inversions/count-inversions.py
@@ -1,16 +1,28 @@
 class Solution:
     def inversionCount(self, arr):
-        # Code Here
+        # code here
+        n=len(arr)
+        def merge_sort(l,r):
+            if l>=r:
+                return 0
+            mid=(l+r)//2
+            cnt=0
+            cnt+=merge_sort(l,mid)
+            cnt+=merge_sort(mid+1,r)
+            cnt+=merge(l,mid,r)
+            return cnt
         def merge(l,mid,r):
             low,high=l,mid+1
-            cnt=0
-            while low<=mid:
-                while high<=r and arr[low]>arr[high]:
-                    high+=1
-                cnt+=(high-(mid+1))
-                low+=1
-            low,high=l,mid+1
+            tot=0
+            j=mid+1
             lst=[]
+            while low<=mid:
+                while j<=r and arr[low]>arr[j]:
+                    j+=1
+                low+=1
+                tot+=(j-(mid+1))
+            # print(tot)
+            low,high=l,mid+1
             while low<=mid and high<=r:
                 if arr[low]<=arr[high]:
                     lst.append(arr[low])
@@ -26,13 +38,5 @@ class Solution:
                 high+=1
             for i in range(l,r+1):
                 arr[i]=lst[i-l]
-            return cnt
-        def mergesort(l,r):
-            if l>=r:
-                return 0
-            mid=(l+r)//2
-            cnt=mergesort(l,mid)
-            cnt+=mergesort(mid+1,r)
-            cnt+=merge(l,mid,r)
-            return cnt
-        return mergesort(0,len(arr)-1)
+            return tot
+        return merge_sort(0,n-1)
