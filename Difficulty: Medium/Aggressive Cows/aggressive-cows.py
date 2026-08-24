4e4adbe668d083e75805ffd901501b99436aa718
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2026-08-24 21:20:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.py
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.py
@@ -1,21 +1,24 @@
 class Solution:
-
-    def aggressiveCows(self, stalls, k):
-        # your code here
-        def assign_stalls(arr,ele):
-            n=len(arr)
-            prev,cnt=arr[0],1
+    def aggressiveCows(self, arr, k):
+        # code here
+        n=len(arr)
+        def position_crows(dist):
+            cur=arr[0]
+            tot_crows=1
             for i in range(1,n):
-                if arr[i]-prev>=ele:
-                    cnt+=1
-                    prev=arr[i]
-            return cnt
-        stalls.sort()
-        l,r=0,max(stalls)-min(stalls)
+                if arr[i]-cur>=dist:
+                    # print("in",arr[i],cur)
+                    tot_crows+=1
+                    cur=arr[i]
+            return tot_crows
+        arr.sort()
+        l,r=0,arr[-1]-arr[0]
         while l<=r:
             mid=(l+r)//2
-            if assign_stalls(stalls,mid)>=k:
+            # print(mid,position_crows(mid))
+            if position_crows(mid)>=k:
                 l=mid+1
             else:
                 r=mid-1
         return r
+        
